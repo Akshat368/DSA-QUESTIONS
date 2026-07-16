cf1ef568aeddc6dee2a3857753f732cd8fdc97c6
Akshat Gupta <www.akshat56@gmail.com>
2026-07-16 15:44:34 +0530

    Time: 847 ms (21.58%), Space: 98.4 MB (26.29%) - LeetHub

---
+++ 1352-product-of-the-last-k-numbers/1352-product-of-the-last-k-numbers.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class ProductOfNumbers {
+private:
+    vector<int> nums;
+
+public:
+    ProductOfNumbers() {}
+
+    void add(int num) {
+        nums.push_back(num);
+    }
+
+    int getProduct(int k) {
+        int product = 1;
+        int n = nums.size();
+
+        for (int i = n - k; i < n; i++) {
+            product *= nums[i];
+        }
+
+        return product;
+    }
+};
